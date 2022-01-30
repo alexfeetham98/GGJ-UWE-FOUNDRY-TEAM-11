eae6bf80885494a4c52d8c3219d3b21955d34a7f
Wojtek <Wojciech2.Chmielewski@live.uwe.ac.uk>
2022-01-30 15:52:03 +0000

    finished menu and added colour for switching on and off

--- GGJ2022-Jamsters-Paradise/Assets/Scripts/Building.cs
+++ GGJ2022-Jamsters-Paradise/Assets/Scripts/Building.cs
@@ -9,10 +9,18 @@ public class Building : MonoBehaviour
     public GameObject buildingBase;
     public GameObject buildingHead;
     public float energyCost;
+
+    public Material activeMat;
+    public Material inactiveMat;
+
+    private MeshRenderer baseRenderer;
     // Start is called before the first frame update
    public void Start()
     {
-
+        if (buildingBase != null)
+        {
+            baseRenderer = buildingBase.GetComponent<MeshRenderer>();
+        }
     }
 
     // Update is called once per frame
@@ -24,6 +32,14 @@ public class Building : MonoBehaviour
     public void SwitchPower()
     {
         isActive = (isActive == true) ? false : true;
+        if (isActive)
+        {
+            baseRenderer.material = activeMat;
+        }
+        else
+        {
+            baseRenderer.material = inactiveMat;
+        }
     }
 
     public virtual void Interact()
